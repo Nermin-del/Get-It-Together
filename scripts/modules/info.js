f78c75bab4a2037a1aa4ccaca14e7f8621cffdeb
Nermin Festa <nermin_festa@hotmail.com>
2022-04-08 14:10:01 +0200

    fix

--- scripts/modules/info.js
+++ scripts/modules/info.js
@@ -1,9 +1,19 @@
 const info = `
 <h1>Info - Get It Together</h1>
-<p>Get It Together är en idè som är framtagen av en helt vanlig individ med passion för träning och hälsa.
- Idèn kom när skaparen hade bekymmer att starta igång sin kampsportskarriär och insåg att fler människor
- delade samma problem. Get It Together vill bryta denna normen att vara rädd för att börja,
- det ska vara lätt att börja och roligt att fortsätta. Det är visionen för idèn. Av kampsportare, för kampsportare.</p>
+<p>Alla människor har drömt någon gång om att börja utöva kampsport efter att ha sett sin förebild på filmduken, eller kanske har man sett någon influens utöva det som man ser som förebild? Men första steget brukar alltid vara det svåraste att ta. Speciellt när vi inte får den informationen som krävs för att våga ta det steget. 
+
+Get It Together löser dom problemen och hjälper dig att ta det jobbiga steget in till en värld full av möjligheter och gemenskap. 
+Det är enkelt att komma igång! Bara skriv in ditt namn och ort och Get It Together hjälper dig att samla alla klubbar som är nära dig så du kan snabbt och enkelt ta kontakt med tränarna och se vad som passar dig bäst!
+
+Våran inkomst baseras på reklam från andra företag. 
+
+Vad vi siktar på är människor i alla åldrar, tjej, kille, kvinna, man det finns inga krav på hur man är eller ska vara. Vem som helst kan börja sin resa till en bättre sida av sig själv.
+
+Ingen vill känna sig dum och förvirrad för att man inte kan navigera sig igenom rätt klubb. Så låt oss på Get It Together lösa den tråkiga biten så du kan lägga mer tid på det roliga!
+
+Get It Toghether, webblösningen för din trygghet.
+</p>
 `;
 
 export default info;
+
--- scripts/modules/projectidea.js
+++ scripts/modules/projectidea.js
@@ -1,19 +1,15 @@
 const projectidea = `
 <h1>Projektidè</h1>
-<p>Alla människor har drömt någon gång om att börja utöva kampsport efter att ha sett sin förebild på filmduken, eller kanske har man sett någon influens utöva det som man ser som förebild? Men första steget brukar alltid vara det svåraste att ta. Speciellt när vi inte får den informationen som krävs för att våga ta det steget. 
+<p>Get It Together är en idè som är framtagen av en helt vanlig individ med passion för träning och hälsa.
+Idèn kom när skaparen hade bekymmer att starta igång sin kampsportskarriär och insåg att fler människor
+delade samma problem. Get It Together vill bryta denna normen att vara rädd för att börja,
+det ska vara lätt att börja och roligt att fortsätta. Det är visionen för idèn. Av kampsportare, för kampsportare.
 
-Get It Together löser dom problemen och hjälper dig att ta det jobbiga steget in till en värld full av möjligheter och gemenskap. 
-Det är enkelt att komma igång! Bara skriv in ditt namn och ort och Get It Together hjälper dig att samla alla klubbar som är nära dig så du kan snabbt och enkelt ta kontakt med tränarna och se vad som passar dig bäst!
-
-Våran inkomst baseras på reklam från andra företag. 
-
-Vad vi siktar på är människor i alla åldrar, tjej, kille, kvinna, man det finns inga krav på hur man är eller ska vara. Vem som helst kan börja sin resa till en bättre sida av sig själv.
-
-Ingen vill känna sig dum och förvirrad för att man inte kan navigera sig igenom rätt klubb. Så låt oss på Get It Together lösa den tråkiga biten så du kan lägga mer tid på det roliga!
-
-Get It Toghether, webblösningen för din trygghet.
-
-Vi finns på appstore och google play.</p>
+Sidan/applikation ska kortfattat användas som ett medium för både klubbar och användare som vill börja kampsporta.
+Den ska vara simpel struktuerad med inloggning, registrering, klubbhantering, träningshantering m.m.
+Sidan ska ha möjlighet att kunna ge information och kommunicera med andra klubbar och användare.
+Sidan ska vara responsiv och mobilanpassad, det ska vara möjligt att använda den på en telefon, en tablet eller en PC.
+</p>
 `;
 
 export default projectidea;
