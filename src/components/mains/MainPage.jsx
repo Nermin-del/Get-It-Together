a9b5995363cedf1570dcde4b903b1fb61a826394
Nermin Festa <nermin_festa@hotmail.com>
2022-04-21 16:32:25 +0200

    minor fixes

--- src/components/mains/MainPage.jsx
+++ src/components/mains/MainPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Nav } from './Nav';
 import { AddClub } from '../clubs/AddClub';
+import { DeleteClub } from '../clubs/DeleteClub';
 import { Footer } from './Footer';
-import { Route } from 'react-router-dom';
-import { Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+
 
 
 export const MainPage = () => {
